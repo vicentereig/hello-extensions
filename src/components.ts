0257fb3909757ab9154cffdf0d79ab9f93deff45
Vicente Reig Rincon de Arellano <vicente.reig@gmail.com>
2024-09-19 16:16:45 +0200

    links stylesheet

--- src/components.ts
+++ src/components.ts
@@ -4,7 +4,7 @@ import {customElement} from 'lit/decorators.js';
 @customElement('scraper-screen')
 export class ScraperScreen extends LitElement {
     render() {
-        return html`<div class="text-center w-1/3 border-2 border-gray-100">
+        return html`<div class="text-center w-1/2 border-2 border-gray-100 bg-white">
             <h1 class="text-3xl font-bold">
                 Oh, Sheet!
             </h1>        
--- src/sites/github.ts
+++ src/sites/github.ts
@@ -7,3 +7,11 @@ let script = document.createElement('script');
 script.src = chrome.runtime.getURL('components.js');
 script.type = 'module';
 document.head.appendChild(script);
+
+//         <link rel="stylesheet" type="text/css" href="../style.css">
+let linkRel = document.createElement('link');
+linkRel.rel = 'stylesheet';
+linkRel.type = 'text/css';
+linkRel.href = '../style.css';
+
+document.head.appendChild(linkRel);
